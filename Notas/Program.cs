38d7d31278cc54fe158edb4850894fba19e5c3e3
hugo de araujo <hugo.saraujo@tutanota.com>
2023-08-01 21:10:59 -0300

    Adicionar arquivos de projeto.

---
+++ Notas/Program.cs
@@ -0,0 +1,21 @@
+﻿var notasAlunos = new Dictionary<string, Dictionary<string, List<int>>> {
+    { "Ana", new Dictionary<string, List<int>> {
+        { "C#", new List<int> { 8, 7, 6 } },
+        { "Java", new List<int> { 7, 6, 5 } },
+        { "Python", new List<int> { 9, 8, 8 } }
+    }},
+    { "Maria", new Dictionary<string, List<int>> {
+        { "C#", new List<int> { 6, 5, 4 } },
+        { "Java", new List<int> { 8, 7, 6 } },
+        { "Python", new List<int> { 6, 10, 5 } }
+    }},
+    { "Luiza", new Dictionary<string, List<int>> {
+        { "C#", new List<int> { 2, 3, 10 } },
+        { "Java", new List<int> { 8, 8, 8 } },
+        { "Python", new List<int> { 7, 7, 7 } }
+    }}
+};
+
+List<int> notasEstudanteMateria = notasAlunos["Ana"]["Python"];
+double mediaNotasPorMateria = notasEstudanteMateria.Average();
+Console.WriteLine(mediaNotasPorMateria);
